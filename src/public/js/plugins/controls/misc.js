737dd5ef7f05dc5a19553250b6870718eb4206d3
Drigtime <william.varlet8020@gmail.com>
2018-10-17 19:36:55 +0200

    cleaning files

--- src/public/js/plugins/controls/misc.js
+++ src/public/js/plugins/controls/misc.js
@@ -1,149 +1,136 @@
 /*
  * L.Control.Miscellaneous is used for displaying toggling miscellaneous resource buttons.
  */
-import sizeOf from "image-size";
-import { dofusCoordsToGeoCoords, map, json, hint, mcgLayerSupportGroup } from "../map";
+import L from 'leaflet';
+import sizeOf from 'image-size';
+import { dofusCoordsToGeoCoords, map, json, hint, mcgLayerSupportGroup } from '../map';
 
-const path = require("path");
-const fs = require("fs");
+const path = require('path');
+const fs = require('fs');
 
 L.Control.Miscellaneous = L.Control.extend({
-    options: {
-        position: "topleft"
-    },
+  options: {
+    position: 'topleft',
+  },
 
-    onAdd(map) {
-        this._menuToggle = false;
-        this._visible = [];
-        this.prof = "misc";
-        this.resources = ["Misc", "Class", "Dungeon", "Lair", "Market", "Workshop", "Water"];
-        const className = "leaflet-control-miscellaneous container-resources";
-        const container = (this._container = L.DomUtil.create("div", className));
-        container.style.marginTop = "0px";
-        L.DomEvent.addListener(container, "dblclick", L.DomEvent.stop);
-        L.DomEvent.addListener(container, "click", L.DomEvent.stop);
-        L.DomEvent.addListener(container, "mousemove", L.DomEvent.stop);
-        const options = this.options;
+  onAdd() {
+    this.menuToggle = false;
+    this.visible = [];
+    this.prof = 'misc';
+    this.resources = ['Misc', 'Class', 'Dungeon', 'Lair', 'Market', 'Workshop', 'Water'];
+    const className = 'leaflet-control-miscellaneous container-resources';
+    this.container = L.DomUtil.create('div', className);
+    const container = this.container;
+    container.style.marginTop = '0px';
+    L.DomEvent.addListener(container, 'dblclick', L.DomEvent.stop);
+    L.DomEvent.addListener(container, 'click', L.DomEvent.stop);
+    L.DomEvent.addListener(container, 'mousemove', L.DomEvent.stop);
 
-        this._aButton = L.DomUtil.create("a", "misc-control dark", container);
-        this._aButton.setAttribute("href", "#");
-        this._aButton.setAttribute("title", "Divers");
-        this._aButton.style.borderTop = "2px solid #212529";
-        this._imgButton = L.DomUtil.create("img", "no-class", this._aButton);
-        this._imgButton.setAttribute("src", "./data/assets/misc/Hint.png");
+    this.aButton = L.DomUtil.create('a', 'misc-control dark', container);
+    this.aButton.setAttribute('href', '#');
+    this.aButton.setAttribute('title', 'Divers');
+    this.aButton.style.borderTop = '2px solid #212529';
+    this.imgButton = L.DomUtil.create('img', 'no-class', this.aButton);
+    this.imgButton.setAttribute('src', './data/assets/misc/Hint.png');
 
-        L.DomEvent.addListener(this._aButton, "click", this._click, this);
+    L.DomEvent.addListener(this.aButton, 'click', this.click, this);
 
-        map.on("move", this._close, this);
-        map.on("click", this._close, this);
+    map.on('move', this.close, this);
+    map.on('click', this.close, this);
 
-        this._magicContainer = L.DomUtil.create("div", "container-resources-magic", container);
+    this.magicContainer = L.DomUtil.create('div', 'container-resources-magic', container);
 
-        this._buttons = [];
-        const resourcesLength = this.resources.length;
-        for (let i = 0; i < resourcesLength; i++) {
-            const aMagicButton = L.DomUtil.create("a", `scale-border-in-out ${this.resources[i]}`, this._magicContainer);
-            aMagicButton.setAttribute("href", "#");
-            // aMagicButton.setAttribute('title', i18next.t(this.prof + "." + this.resources[i]));
-            const imgMagicButton = L.DomUtil.create("img", "no-class", aMagicButton);
-            imgMagicButton.setAttribute("src", path.join(__dirname, `../../../../data/assets/misc/${this.resources[i]}.png`));
-            L.DomEvent.addListener(aMagicButton, "click", this._toggle, this);
-        }
-
-        for (let j = 0; j < resourcesLength; j++) {
-            json[this.resources[j]] = JSON.parse(
-                fs.readFileSync(path.join(__dirname, `../../../../data/json/miscellaneous/${this.resources[j]}.json`))
-            );
-            hint[this.resources[j]] = L.layerGroup();
-            for (const key in json[this.resources[j]]) {
-                if (json[this.resources[j]][key].worldMapId === 1) {
-                    L.marker(dofusCoordsToGeoCoords([json[this.resources[j]][key].posX, json[this.resources[j]][key].posY]), {
-                        icon: L.icon({
-                            iconUrl: path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`),
-                            iconAnchor: [
-                                sizeOf(path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`)).width / 2,
-                                sizeOf(path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`)).height / 2
-                            ]
-                        }),
-                        interactive: false
-                    }).addTo(hint[this.resources[j]]);
-                }
-            }
-            mcgLayerSupportGroup.checkIn([hint[this.resources[j]]]);
-        }
-        return container;
-    },
-
-    _click(evt) {
-        if (evt.ctrlKey) {
-            for (let i = 0; i < this.resources.length; i++) {
-                if (this._visible[i]) {
-                    continue;
-                }
-                $(this._magicContainer.children[i])[0].click();
-            }
-            return;
-        }
-        if (evt.altKey) {
-            for (let j = 0; j < this.resources.length; j++) {
-                if (this._visible[j]) {
-                    $(this._magicContainer.children[j])[0].click();
-                } else {
-                    continue;
-                }
-            }
-            return;
-        }
-        if (this._menuToggle === false) {
-            L.DomUtil.addClass(this._aButton, "container-resources-open");
-            L.DomUtil.addClass(this._magicContainer, "in");
-            this._menuToggle = true;
-        } else {
-            this._close();
-        }
-    },
-    _close() {
-        L.DomUtil.removeClass(this._magicContainer, "in");
-        L.DomUtil.removeClass(this._aButton, "container-resources-open");
-        this._menuToggle = false;
-    },
-    _toggle(a) {
-        for (let i = 0; i < this.resources.length; i++) {
-            if (L.DomUtil.hasClass(a.currentTarget, this.resources[i])) {
-                if (this._visible[i]) {
-                    hint[this.resources[i]].removeFrom(map);
-                    L.DomUtil.removeClass(a.currentTarget, "selected");
-                    this._visible[i] = false;
-                } else {
-                    hint[this.resources[i]].addTo(map);
-                    L.DomUtil.addClass(a.currentTarget, "selected");
-                    this._visible[i] = true;
-                }
-                break;
-            }
-        }
-    },
-
-    onRemove(map) {
-        map.off("move", this._close, this);
-        map.off("click", this._close, this);
-        // todo remove toggle listeners?
+    this.buttons = [];
+    const resourcesLength = this.resources.length;
+    for (let i = 0; i < resourcesLength; i += 1) {
+      const aMagicButton = L.DomUtil.create('a', `scale-border-in-out ${this.resources[i]}`, this.magicContainer);
+      aMagicButton.setAttribute('href', '#');
+      // aMagicButton.setAttribute('title', i18next.t(this.prof + "." + this.resources[i]));
+      const imgMagicButton = L.DomUtil.create('img', 'no-class', aMagicButton);
+      imgMagicButton.setAttribute('src', path.join(__dirname, `../../../../data/assets/misc/${this.resources[i]}.png`));
+      L.DomEvent.addListener(aMagicButton, 'click', this.toggle, this);
     }
+
+    for (let j = 0; j < resourcesLength; j += 1) {
+      json[this.resources[j]] = JSON.parse(
+        fs.readFileSync(path.join(__dirname, `../../../../data/json/miscellaneous/${this.resources[j]}.json`)),
+      );
+      hint[this.resources[j]] = L.layerGroup();
+      Object.keys(json[this.resources[j]]).forEach((key) => {
+        L.marker(dofusCoordsToGeoCoords([json[this.resources[j]][key].posX, json[this.resources[j]][key].posY]), {
+          icon: L.icon({
+            iconUrl: path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`),
+            iconAnchor: [
+              sizeOf(path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`)).width / 2,
+              sizeOf(path.join(__dirname, `../../../../data/assets/hint/${json[this.resources[j]][key].gfx}.png`)).height / 2,
+            ],
+          }),
+          interactive: false,
+        }).addTo(hint[this.resources[j]]);
+      });
+      mcgLayerSupportGroup.checkIn([hint[this.resources[j]]]);
+    }
+    return container;
+  },
+
+  click(evt) {
+    if (evt.ctrlKey) {
+      for (let i = 0; i < this.resources.length; i += 1) {
+        if (this.visible[i]) {
+          continue;
+        }
+        $(this.magicContainer.children[i])[0].click();
+      }
+      return;
+    }
+    if (evt.altKey) {
+      for (let j = 0; j < this.resources.length; j += 1) {
+        if (this.visible[j]) {
+          $(this.magicContainer.children[j])[0].click();
+        } else {
+          continue;
+        }
+      }
+      return;
+    }
+    if (this.menuToggle === false) {
+      L.DomUtil.addClass(this.aButton, 'container-resources-open');
+      L.DomUtil.addClass(this.magicContainer, 'in');
+      this.menuToggle = true;
+    } else {
+      this.close();
+    }
+  },
+  close() {
+    L.DomUtil.removeClass(this.magicContainer, 'in');
+    L.DomUtil.removeClass(this.aButton, 'container-resources-open');
+    this.menuToggle = false;
+  },
+  toggle(a) {
+    for (let i = 0; i < this.resources.length; i += 1) {
+      if (L.DomUtil.hasClass(a.currentTarget, this.resources[i])) {
+        if (this.visible[i]) {
+          hint[this.resources[i]].removeFrom(map);
+          L.DomUtil.removeClass(a.currentTarget, 'selected');
+          this.visible[i] = false;
+        } else {
+          hint[this.resources[i]].addTo(map);
+          L.DomUtil.addClass(a.currentTarget, 'selected');
+          this.visible[i] = true;
+        }
+        break;
+      }
+    }
+  },
+
+  onRemove() {
+    map.off('move', this.close, this);
+    map.off('click', this.close, this);
+    // todo remove toggle listeners?
+  },
 });
 
 // constructor registration
-L.control.miscellaneous = function(options) {
-    return new L.Control.Miscellaneous(options);
+L.control.miscellaneous = function (options) {
+  return new L.Control.Miscellaneous(options);
 };
-
-// map init hook
-L.Map.mergeOptions({
-    miscellaneousControl: false
-});
-
-L.Map.addInitHook(function() {
-    if (this.options.miscellaneousControl) {
-        this.miscellaneousControl = new L.Control.Miscellaneous();
-        this.addControl(this.miscellaneousControl);
-    }
-});
