c988e1334b4ae7f32f0cd1f83a564a94ae608083
Drigtime <william.varlet8020@gmail.com>
2018-10-22 20:52:37 +0200

    little code changes

--- src/public/js/events/config.js
+++ src/public/js/events/config.js
@@ -1,5 +1,5 @@
 import settings from '../../../data/json/settings';
-import { autoDelete, elementToGather, lifeMinMax, monsterForbidden, monsterMandatory, monsterQuantMinMax, regenItems } from './htmlElementInstance';
+import { elementWithAutoComplete, lifeMinMax, monsterQuantMinMax } from './htmlElementInstance';
 import { putGetItem } from '../scripts/loadPath';
 
 $('#addPutItem').on('click', () => {
@@ -13,30 +13,36 @@ $('#addGetItem').on('click', () => {
 $('#ConfigBtnDefault').on('click', () => {
   $('#displayGatherCountCheckbox').prop('checked', false);
   $('#openBagsCheckbox').prop('checked', false);
-  Object.keys(elementToGather.chipsData).forEach((chip) => {
-    elementToGather.deleteChip(chip);
+  Object.keys(elementWithAutoComplete.elementToGather[0].chipsData).forEach((chip) => {
+    elementWithAutoComplete.elementToGather[0].deleteChip(chip);
   });
   $('#displayFightCountCheckbox').prop('checked', false);
   $('#autoRegenCheckbox').prop('checked', true);
-  lifeMinMax.noUiSlider.set([settings.script.config.AUTO_REGEN.minLife, settings.script.config.AUTO_REGEN.maxLife]);
-  Object.keys(regenItems.chipsData).forEach((chip) => {
-    regenItems.deleteChip(chip);
+  lifeMinMax.noUiSlider.set([
+    settings.script.config.AUTO_REGEN.minLife,
+    settings.script.config.AUTO_REGEN.maxLife,
+  ]);
+  Object.keys(elementWithAutoComplete.regenItems[0].chipsData).forEach((chip) => {
+    elementWithAutoComplete.regenItems[0].deleteChip(chip);
   });
   $('#regenItemValue').val(settings.script.config.AUTO_REGEN.store);
-  monsterQuantMinMax.noUiSlider.set([settings.script.config.MIN_MONSTERS, settings.script.config.MAX_MONSTERS]);
+  monsterQuantMinMax.noUiSlider.set([
+    settings.script.config.MIN_MONSTERS,
+    settings.script.config.MAX_MONSTERS,
+  ]);
   $('#monsterMin').val(settings.script.config.MIN_MONSTERS_LEVEL);
   $('#monsterMax').val(settings.script.config.MAX_MONSTERS_LEVEL);
-  Object.keys(monsterForbidden.chipsData).forEach((chip) => {
-    monsterForbidden.deleteChip(chip);
+  Object.keys(elementWithAutoComplete.monsterForbidden[0].chipsData).forEach((chip) => {
+    elementWithAutoComplete.monsterForbidden[0].deleteChip(chip);
   });
-  Object.keys(monsterMandatory.chipsData).forEach((chip) => {
-    monsterMandatory.deleteChip(chip);
+  Object.keys(elementWithAutoComplete.monsterMandatory[0].chipsData).forEach((chip) => {
+    elementWithAutoComplete.monsterMandatory[0].deleteChip(chip);
   });
   $('#maxFightPerMapCheckbox').prop('checked', false);
   $('#maxFightPerMapValue').val(settings.script.config.MAX_FIGHTS_PER_MAP);
   $('#maxPods').val(settings.script.config.MAX_PODS);
-  Object.keys(autoDelete.chipsData).forEach((chip) => {
-    autoDelete.deleteChip(chip);
+  Object.keys(elementWithAutoComplete.autoDelete[0].chipsData).forEach((chip) => {
+    elementWithAutoComplete.autoDelete[0].deleteChip(chip);
   });
   $('#putKamasCheckbox').prop('checked', false);
   $('#putKamasValue').val(settings.script.config.BANK_PUT_KAMAS);
@@ -50,4 +56,3 @@ $('#ConfigBtnDefault').on('click', () => {
   $('#scriptArea').val('');
   $('#scriptJob').val('');
 });
-
--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,6 @@
-// import './public/js/i18n.js';
+import './public/js/i18n.js';
 import './public/js/events/settings';
 import './public/js/events/config';
 import './public/js/events/map';
 import './public/js/events/titlebar';
 import './public/js/events/tools';
-
-// $(document).on('keyup', (e) => {
-//   if (e.ctrlKey && e.key === 'n') {
-//     $('#NewFile').click();
-//   } else if (e.ctrlKey && e.key === 'o') {
-//     $('#LoadFile').click();
-//   } else if (e.ctrlKey && e.key === 's') {
-//     $('#SaveFile').click();
-//   } else if (e.ctrlKey && e.key === 'r') {
-//     $('#RealoadApp').click();
-//   } else if (e.ctrlKey && e.shitKey && e.key === 'i') {
-//     $('#DevTools').click();
-//   }
-// });
