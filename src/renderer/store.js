4b78a51579ab23c52e794ea1564e319bf34627a2
VARLET William <wvarlet@cr-picardie.net>
2019-01-28 09:23:26 +0100

    fix redux action

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -1,15 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose  } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "renderer/reducers";
 
 const initialState = {};
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  (applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  composeEnhancers(applyMiddleware(...middleware)),
 );
+
 export default store;
